58457b8c3b7df17aa66c1956911ff537859280fb
omutanen <omutanen@student.uef.fi>
2025-03-25 17:19:31 +0200

    Connected DayProgressManager and OpenSign together

--- flask/scripts/day_progress/day_progress_manager.gd
+++ flask/scripts/day_progress/day_progress_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
-signal time_advanced
+signal time_advanced(day_progress: float)
+signal day_over
 
 var DAY_LENGTH_IN_SECONDS: int = 5
 var elapsedSeconds: int = 0
@@ -10,7 +11,9 @@ func _ready() -> void:
 	secondTimer.timeout.connect(_second_timer_timeout)
 
 func _reached_end_of_day() -> void:
+	elapsedSeconds = DAY_LENGTH_IN_SECONDS
 	secondTimer.stop()
+	day_over.emit()
 
 func _second_timer_timeout() -> void:
 	elapsedSeconds += 1
@@ -30,3 +33,9 @@ func get_day_length() -> int:
 func start_day() -> void:
 	secondTimer.start(1)
 	time_advanced.emit(0)
+
+func end_day_early() -> void:
+	if !is_day_over():
+		_reached_end_of_day()
+		time_advanced.emit(get_day_progress())
+		
--- flask/scripts/main/main.gd
+++ flask/scripts/main/main.gd
@@ -1,4 +1,11 @@
 extends Node
 
 func _ready() -> void:
-	pass
+	$DayProgressManager.day_over.connect($game_level/OpenSign.close_shop)
+	$game_level/OpenSign.shop_opened_status_changed.connect(_shop_opened_or_closed)
+
+func _shop_opened_or_closed(open: bool) -> void:
+	if open:
+		$DayProgressManager.start_day()
+	else:
+		$DayProgressManager.end_day_early()
