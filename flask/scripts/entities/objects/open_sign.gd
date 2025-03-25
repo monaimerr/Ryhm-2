a909ed3d416280dd015b252f865c28ae94445a89
Otto Koivuniemi <ottokoivuniemi45@gmail.com>
2025-03-25 16:27:13 +0200

    Added visuals to the open/closed sign. Modified open_sign script.

--- flask/scripts/entities/objects/open_sign.gd
+++ flask/scripts/entities/objects/open_sign.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var interactable: Area2D = $Interactable
 var open = false
 var opened_today = false
@@ -11,16 +12,28 @@ func _ready() -> void:
 # opens the shop if the shop is currently closed adn hasn't yet been opened today
 func _on_interact():
 	if !open and !opened_today:
-		open = true
-		opened_today = true
+		open_shop()
 	else:
-		open = false
-	shop_opened_status_changed.emit(open)
+		close_shop()
 	print(open)
 	
 # should be called at the beginning of a new day to reset the state
 func _on_day_changed():
-	open = false
 	opened_today = false
+	interactable.interact_name = "[E] to open the shop"
+	
+func open_shop():
+	open = true
+	opened_today = true
+	animated_sprite_2d.animation = "open"
+	interactable.interact_name = "[E] to close the shop"
+	shop_opened_status_changed.emit(open)
+	
+func close_shop():
+	open = false
+	animated_sprite_2d.animation = "closed"
+	interactable.interact_name = "You can only open the shop once a day!"
+	shop_opened_status_changed.emit(open)
+	
 	
 	
