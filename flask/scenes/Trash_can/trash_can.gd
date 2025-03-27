ba1cb26601c92f4e074ef85a595a02e295560837
omutanen <omutanen@student.uef.fi>
2025-03-27 13:47:01 +0200

    Set up new trash can implementation for Mounaim to build upon

--- flask/scenes/Trash_can/trash_can.gd
+++ flask/scenes/Trash_can/trash_can.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
-# Called when a body enters the area (e.g., when a potion touches the trash can).
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	# Check if the entered body is a potion (make sure potions are in the "potion" group)
-	if body.is_in_group("potion"):
-		print("Potion entered the trash can area!")
-		# You could also add some interaction here to highlight or mark the potion for removal
+# This is the trash can's interaction handler
+# It has an "interact_name", which is the text shown when the player can
+# interact with the interaction handler
+# It also has functions "interact" and "interact2" for handling two different
+# kinds of interactions
+# You overwrite these with your own values to customize
+@onready var interactable = $Interactable
 
-# Called when the user clicks on the trash can (input event)
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	# Detect if the mouse was clicked
-	if event is InputEventMouseButton and event.pressed:
-		var inv = InventoryManager.playerInv  # Reference to player's inventory
-		if inv.size() > 0:  # Ensure the inventory has items
-			var potion_to_remove = inv[0]  # Example: Remove the first potion (could be customized)
-			InventoryManager.remove_item(potion_to_remove)  # Remove the potion from the inventory
-			print(potion_to_remove + " discarded.")  # Print confirmation
+func _ready() -> void:
+	# Show this text when the player can interact with the interaction handler
+	interactable.interact_name = "[E] to empty your inventory"
+	# When the interaction handler is interacted with, run "_interact"
+	interactable.interact = _interact
+
+func _interact() -> void:
+	var playerInv = InventoryManager.playerInv
