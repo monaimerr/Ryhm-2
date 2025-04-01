9ab5575e4ba75e1548dae65b2bad7c4853ff3bb4
monaimerr <monaim910@gmail.com>
2025-04-01 17:41:40 +0300

    the player can remove the first potion of the inventory after interacting with the trash can

--- flask/scenes/Trash_can/trash_can.gd
+++ flask/scenes/Trash_can/trash_can.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
-# This is the trash can's interaction handler
-# It has an "interact_name", which is the text shown when the player can
-# interact with the interaction handler
-# It also has functions "interact" and "interact2" for handling two different
-# kinds of interactions
-# You overwrite these with your own values to customize
 @onready var interactable = $Interactable
+var playerInv = InventoryManager.playerInv
+var selected_index = 0  
 
 func _ready() -> void:
-	# Show this text when the player can interact with the interaction handler
-	interactable.interact_name = "[E] to empty your inventory"
-	# When the interaction handler is interacted with, run "_interact"
+	interactable.interact_name = "[E] to delete an item"
 	interactable.interact = _interact
 
 func _interact() -> void:
-	var playerInv = InventoryManager.playerInv
+	if playerInv.size() > 0:
+		var item_to_remove = playerInv[selected_index]
+		InventoryManager.remove_item(item_to_remove)
+		print("Removed: " + str(item_to_remove))
+		
+		if playerInv.size() == 0:
+			print("Inventory is now empty!")
+	else:
+		print("Inventory is already empty!")
